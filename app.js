afed67598ee847bb3d0c124c5ff884782143d37a
starmickey <micaelaestrellamorales@gmail.com>
2023-02-17 10:12:14 -0300

    Register users creating session cookies

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongooseInterface = require(__dirname + '/mongoose.js');
-
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 
 /* =========== CONFIG EXPRESS APP =========== */
@@ -12,6 +15,55 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// ================ CONNECT TO MONGOOSE ================
+
+startMongooseConnection().catch(err => console.log(err));
+
+async function startMongooseConnection() {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect('mongodb://127.0.0.1:27017/secrets');
+}
+
+
+// ================== MODELS & SCHEMAS ==================
+
+const userSchema = new mongoose.Schema({
+    active: Boolean
+});
+
+userSchema.plugin(passportLocalMongoose, {
+    // Set usernameUnique to false to avoid a mongodb index on the username column!
+    usernameUnique: false,
+
+    findByUsername: function (model, queryParameters) {
+        // Restrict users search to find only active ones
+        queryParameters.active = true;
+        return model.findOne(queryParameters);
+    }
+});
+
+const User = mongoose.model('user', userSchema);
+
+
+/*  ---------- Create user sessions ---------- */
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 
 /* ========= HTTP REQUESTS' HANDLERS ========= */
@@ -26,36 +78,39 @@ app.route('/register')
     })
 
     .post(function (req, res) {
-        mongooseInterface.registerUser(req.body.username, req.body.password)
-            .then(
-                function onfullfilled(value) {
-                    res.render('secrets');
-                },
-                function onrejected(reason) {
-                    console.log(reason);
-                    res.render('register');
-                }
-            )
+
+        User.register({ username: req.body.username, active: true }, req.body.password, function (err, user) {
+            if (err) {
+                console.log(err);
+                res.redirect('/register');
+            } else {
+                console.log('user registered');
+                passport.authenticate('local')(req, res, function(){
+                    res.redirect('/secrets');
+                });
+            }
+
+        });
     });
 
+    
 app.route('/login')
     .get(function (req, res) {
         res.render('login');
     })
     .post(function (req, res) {
-        mongooseInterface.login(req.body.username, req.body.password)
-            .then(
-                function onfullfilled(value) {
-                    res.render('secrets');
-                },
-                function onrejected(reason) {
-                    console.log(reason);
-                    res.render('login');
-                }
-            )
     });
 
 
+app.get('/secrets', function (req, res) {
+    if (req.isAuthenticated()) {
+        console.log('secrets authentication success');
+        res.render('secrets');
+    } else {
+        console.log('secrets authentication fault');
+        res.redirect('/login');
+    }
+})
 
 
 /* ============ PORT LISTENER ============ */
